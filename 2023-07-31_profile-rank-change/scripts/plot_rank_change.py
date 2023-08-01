34ae3660dc345189646f870a9c8ddbca0e13cb1a
rogerhh <rogerhh@umich.edu>
2023-07-31 23:50:23 -0700

    Plots

--- 2023-07-31_profile-rank-change/scripts/plot_rank_change.py
+++ 2023-07-31_profile-rank-change/scripts/plot_rank_change.py
@@ -11,13 +11,16 @@ if __name__ == "__main__":
     parser.add_option("--var_width", dest="var_width", 
                       default="", help="The width of the variables (assuming same for all)")
     parser.add_option("--output_figure", dest="output_figure", 
-                      default="", help="The width of the variables (assuming same for all)")
+                      default="", help="The filename for hte output graph")
+    parser.add_option("--select_period", dest="select_period", 
+                      default="200", help="The period at which to select 'loop closure steps'")
 
     (options, args) = parser.parse_args()
 
     filename = options.input_file
     var_width = int(options.var_width)
     out_figure = options.output_figure
+    select_period = int(options.select_period)
 
     output = {}
 
@@ -43,4 +46,37 @@ if __name__ == "__main__":
     plt.plot(output["Step"], output["Depth"], "r")
     plt.legend(["Step", "Loop Closure Size"])
     plt.title("Step vs Loop Closure Size")
-    plt.savefig(out_figure)
+
+    if out_figure != "":
+        plt.savefig(out_figure + ".png")
+
+    # Select the highest percentage depth every select_period steps
+    max_steps = []
+    max_depths = []
+    max_is = []
+    for start_i in range(0, len(output["Step"]), select_period):
+        max_percentage_depth = 0
+        max_step = 0
+        max_i = 0
+        max_depth = 0
+        for i in range(start_i, min(start_i + select_period, len(output["Step"]))):
+            depth = output["Depth"][i]
+            step = output["Step"][i]
+            percentage_depth = depth / step            
+            if percentage_depth >= max_percentage_depth:
+                max_percentage_depth = percentage_depth
+                max_step = step
+                max_i = i
+                max_depth = depth
+        max_steps.append(max_step)
+        max_is.append(max_i)
+        max_depths.append(max_depth)
+
+    print(max_steps)
+    print(max_depths)
+
+    plt.plot(max_steps, max_depths, 'ro', markersize=10, mfc='none')
+    if out_figure != "":
+        plt.savefig(out_figure + "_selected.png")
+
+            
--- 2023-07-31_profile-rank-change/scripts/plot_rank_change.sh
+++ 2023-07-31_profile-rank-change/scripts/plot_rank_change.sh
@@ -8,4 +8,4 @@ OUTPUT_DIR=$EXPERIMENT_DIR/output
 PROCESSED_DIR=$EXPERIMENT_DIR/output/processed
 DATA_DIR=$EXPERIMENT_DIR/data
 
-python3 $SCRIPT_DIR/plot_rank_change.py --input_file $OUTPUT_DIR/w10000_rank_change.out --var_width 3 --output_figure $DATA_DIR/w10000_step_vs_change_rank.png
+python3 $SCRIPT_DIR/plot_rank_change.py --input_file $OUTPUT_DIR/w10000_rank_change.out --var_width 3 --output_figure $DATA_DIR/w10000_step_vs_change_rank --select_period 200
