bc2a1979b8000991ae5d7cebe6cf273339479a79
rogerhh <rogerhh@umich.edu>
2024-01-13 22:24:25 +0000

    add sphere results

--- 2024-01-13_node-size-distribution/scripts/run_experiment.sh
+++ 2024-01-13_node-size-distribution/scripts/run_experiment.sh
@@ -10,4 +10,6 @@ BUILD_DIR=~/gtsam-gemmini-integrate/build
 
 cd $BUILD_DIR
 
-make -j4 timing && ./timing/testGtsamIncremental -f w10000 --num_steps 10000 --node_dist_file "$OUTPUT_DIR/w10000_node_sizes.csv"
+# make -j4 timing && ./timing/testGtsamIncremental -f w10000 --num_steps 10000 --node_dist_file "$OUTPUT_DIR/w10000_node_sizes.csv"
+
+make -j4 timing && ./timing/testGtsamIncremental3D -f sphere2500 --node_dist_file "$OUTPUT_DIR/sphere2500_node_sizes.csv"
